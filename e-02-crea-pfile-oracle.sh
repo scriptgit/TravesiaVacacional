bb69411a981606146e56fe18d1691ccfa3673a29
JOBA25 <jorge_octavio25@outlook.com>
2024-04-17 18:27:45 -0600

    cambios 2 rutas para usar fra 4 crear carpetas

--- e-02-crea-pfile-oracle.sh
+++ e-02-crea-pfile-oracle.sh
@@ -15,11 +15,6 @@ db_domain=fi.unam
 db_recovery_file_dest_size=20G
 db_recovery_file_dest='/travesia/disk-07/fast-recovery-area'
 db_flashback_retention_target=1440
-control_files=(
-  /travesia/disk-07/fast-recovery-area/control01.ctl,
-  /travesia/disk-02/app/oracle/oradata/${ORACLE_SID^^}/control02.ctl,
-  /travesia/disk-03/app/oracle/oradata/${ORACLE_SID^^}/control03.ctl
-)
 " > ${pfile}
 
 echo "comprobando creación"
@@ -27,4 +22,4 @@ echo ""
 
 echo "-l"
 ls -l ${pfile}
-#cat ${pfile}
+
--- e-04-crea-directorios-root.sh
+++ e-04-crea-directorios-root.sh
@@ -3,9 +3,12 @@ echo "Creando directorios para crear la BD"
 export ORACLE_SID=travdip1
 
 echo "creando directorios para datafiles"
+#ORACLE_HOME = /u01/app/oracle/product/19.3.0/dbhome_1
+#ORACLE_BASE = /u01/app/oracle
 oraData="${ORACLE_BASE}/oradata"
 dirDataFiles="${ORACLE_SID^^}"
 
+echo "comprobando si el directorio de la base ya existe en la carpeta oradata"
 if [ -d "${oraData}/${dirDataFiles}" ]; then
   echo "El directorio existe, contenido: "
   ls -l ${oraData}/${dirDataFiles}
@@ -14,7 +17,7 @@ if [ -d "${oraData}/${dirDataFiles}" ]; then
   rm -f *.dbf
 fi;
 
-echo "creando directorios"
+echo "creando directorios en directorio oradata"
 mkdir -p ${oraData}/${dirDataFiles}
 
 echo "moviendonos a ${oraData}"
@@ -25,37 +28,73 @@ chown oracle:oinstall ${dirDataFiles}
 echo "Cambiando permisos a la carpeta de datafiles"
 chmod 750 ${dirDataFiles}
 
+#carpeta "raiz" del proyecto
+echo "Creando carpeta raiz /travesia"
+raiz=/travesia
+if [ -d "${raiz}" ]; then
+  echo "El directorio existe, contenido: "
+  ls -l ${raiz}
+  read -p "Enter para borrar, ctrl C - cancelar"
+  rm -f ${raiz}
+fi;
 
-echo "Crear Carpteas para Redo logs y Control files"
+mkdir -p ${raiz}
 
-pMontaje1="/travesia/disk-01"
-pMontaje2="/travesia/disk-02"
-pMontaje3="/travesia/disk-03"
+echo "cambiando permisos a la carpeta /travesia"
+echo ""
 
+cd /
+chmod -R 777 travesia
+
+#directorios que simulan los discos
+echo "Crear direcotoriso que simularan puntos de montaje"
+
+pMontaje1="disk-01"
+pMontaje2="disk-02"
+pMontaje3="disk-03"
+pMontaje4="disk-04"
+pMontaje5="disk-05"
+pMontaje6="disk-06"
+pMontaje7="disk-07"
+pMontaje8="disk-08"
+
+cd ${raiz}
+mkdir -p ${pMontaje1}
+mkdir -p ${pMontaje2}
+mkdir -p ${pMontaje3}
+mkdir -p ${pMontaje4}
+mkdir -p ${pMontaje5}
+mkdir -p ${pMontaje6}
+mkdir -p ${pMontaje7}
+mkdir -p ${pMontaje8}
+
+#revisamos solo dos ubicaciones ya que una copia estará en la FRA
 redoData="app/oracle/oradata/${ORACLE_SID^^}"
-
-if [[ -d "${pMontaje1}/${redoData}" ||  -d "${pMontaje2}/${redoData}" || -d "${pMontaje3}/${redoData}" ]]; then
+if [[ -d "${raiz}/${pMontaje1}/${redoData}" ||  -d "${raiz}/${pMontaje2}/${redoData}"  ]]; then
   echo "Directorios de redo logs existen"
   read -p "Enter para eliminar, Ctrl c - cancelar"
-  cd ${pMontaje1}
+  cd ${raiz}/${pMontaje1}
   rm -f ${redoData}/*.log
 
-  cd ${pMontaje2}
+  cd ${raiz}/${pMontaje2}
   rm -f ${redoData}/*.log
 
-  cd ${pMontaje3}
-  rm -f ${redoData}/*.log
 
 fi;
 
 echo "Creando carpetas de redo logs"
 mkdir -p ${pMontaje1}/${redoData}
 mkdir -p ${pMontaje2}/${redoData}
-mkdir -p ${pMontaje3}/${redoData}
 
+
+
+echo "Creando carpetas de FRA"
+fra=/fast-recovery-area
+mkdir -p ${raiz}/${pMontaje7}/${fra}
 
 echo "cambiando permisos a la carpeta de redo logs"
 echo ""
+
 
 cd /travesia
 chown -R oracle:oinstall disk-*
