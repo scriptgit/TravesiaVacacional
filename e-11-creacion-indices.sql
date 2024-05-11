7149816ef6ca295d393d75e066a5d8f6f2491932
angel.oropeza <angel.oropeza@rappi.com>
2024-05-11 14:20:23 -0600

    add index to empleado, asociacion, visita, cliente, membresia tables

--- e-11-creacion-indices.sql
+++ e-11-creacion-indices.sql
@@ -5,4 +5,33 @@
 --@Descripción:
 
 
--- TODO: crear indices y asignarlos a TBs correspondientes
+-- M Ó D U L O:  A D M I N I S T R A C I Ó N --
+
+-- TABLA: EMPLEADO
+CREATE INDEX ix_empleado_clave
+ON empleado(clave)
+TABLESPACE admin_indices_tbs;
+
+-- TABLA: ASOCIACION
+CREATE INDEX ix_asociacion_clave
+ON asociacion(clave)
+TABLESPACE admin_indices_tbs;
+
+
+
+-- M Ó D U L O:  N E G O C I O --
+
+-- TABLA: VISITA
+CREATE INDEX ix_visita_cliente
+ON visita(cliente_id)
+TABLESPACE negocio_indices_tbs;
+
+-- TABLA: CLIENTE
+CREATE INDEX ix_cliente_clave_curp
+ON cliente(clave, curp)
+TABLESPACE negocio_indices_tbs;
+
+-- TABLA: MEMBRESIA
+CREATE INDEX ix_membresia_cliente_numero_membresia
+ON membresia(numero_membresia)
+TABLESPACE negocio_indices_tbs;
