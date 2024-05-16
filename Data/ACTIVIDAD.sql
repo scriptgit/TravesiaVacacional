017f3703a982c75339778d089eb17da258210db0
Ulises <38197638+scriptgit@users.noreply.github.com>
2024-05-16 15:22:58 -0600

    dataset tabla actividad

---
+++ Data/ACTIVIDAD.sql
@@ -0,0 +1,28 @@
+﻿/*CREATE TABLE ACTIVIDAD 
+(
+    actividad_id	INT,
+    clave_actividad_id	INT,
+    descripcion_actividad	VARCHAR(512),
+    costo_actividad	INT
+);*/
+
+INSERT INTO ACTIVIDAD (actividad_id, clave_actividad_id, descripcion_actividad, costo_actividad) VALUES ('1', '31817', 'Basketball', '15000');
+INSERT INTO ACTIVIDAD (actividad_id, clave_actividad_id, descripcion_actividad, costo_actividad) VALUES ('2', '17159', 'Beisbol', '12000');
+INSERT INTO ACTIVIDAD (actividad_id, clave_actividad_id, descripcion_actividad, costo_actividad) VALUES ('3', '64673', 'Calabozos y mazmorras', '10000');
+INSERT INTO ACTIVIDAD (actividad_id, clave_actividad_id, descripcion_actividad, costo_actividad) VALUES ('4', '62104', 'Campamento al aire libre', '20000');
+INSERT INTO ACTIVIDAD (actividad_id, clave_actividad_id, descripcion_actividad, costo_actividad) VALUES ('5', '13979', 'Campamento de halloween', '20000');
+INSERT INTO ACTIVIDAD (actividad_id, clave_actividad_id, descripcion_actividad, costo_actividad) VALUES ('6', '75159', 'Campamento de invierno', '20000');
+INSERT INTO ACTIVIDAD (actividad_id, clave_actividad_id, descripcion_actividad, costo_actividad) VALUES ('7', '84868', 'Campamento de verano', '20000');
+INSERT INTO ACTIVIDAD (actividad_id, clave_actividad_id, descripcion_actividad, costo_actividad) VALUES ('8', '66889', 'Campamento de primavera', '20000');
+INSERT INTO ACTIVIDAD (actividad_id, clave_actividad_id, descripcion_actividad, costo_actividad) VALUES ('9', '16457', 'Boxeo', '12000');
+INSERT INTO ACTIVIDAD (actividad_id, clave_actividad_id, descripcion_actividad, costo_actividad) VALUES ('10', '44604', 'Danza', '10000');
+INSERT INTO ACTIVIDAD (actividad_id, clave_actividad_id, descripcion_actividad, costo_actividad) VALUES ('11', '78623', 'Esqui', '12000');
+INSERT INTO ACTIVIDAD (actividad_id, clave_actividad_id, descripcion_actividad, costo_actividad) VALUES ('12', '36692', 'Futbol', '10000');
+INSERT INTO ACTIVIDAD (actividad_id, clave_actividad_id, descripcion_actividad, costo_actividad) VALUES ('13', '56624', 'Guardabosques', '10000');
+INSERT INTO ACTIVIDAD (actividad_id, clave_actividad_id, descripcion_actividad, costo_actividad) VALUES ('14', '48838', 'Juegos de mesa', '10000');
+INSERT INTO ACTIVIDAD (actividad_id, clave_actividad_id, descripcion_actividad, costo_actividad) VALUES ('15', '69429', 'Juegos de rol', '10000');
+INSERT INTO ACTIVIDAD (actividad_id, clave_actividad_id, descripcion_actividad, costo_actividad) VALUES ('16', '71503', 'Natacion', '10000');
+INSERT INTO ACTIVIDAD (actividad_id, clave_actividad_id, descripcion_actividad, costo_actividad) VALUES ('17', '10262', 'Realidad virtual', '12000');
+INSERT INTO ACTIVIDAD (actividad_id, clave_actividad_id, descripcion_actividad, costo_actividad) VALUES ('18', '22439', 'Supervivencia a la intemperie', '20000');
+INSERT INTO ACTIVIDAD (actividad_id, clave_actividad_id, descripcion_actividad, costo_actividad) VALUES ('19', '74918', 'Tenis', '12000');
+INSERT INTO ACTIVIDAD (actividad_id, clave_actividad_id, descripcion_actividad, costo_actividad) VALUES ('20', '61117', 'Tiro con arco', '12000');
