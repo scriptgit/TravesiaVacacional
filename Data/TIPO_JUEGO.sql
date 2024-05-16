e61cbf1614295bec5cecfffb2dedd5ef0672060d
Ulises <38197638+scriptgit@users.noreply.github.com>
2024-05-16 15:20:00 -0600

    dataset tabla tipo_juego

---
+++ Data/TIPO_JUEGO.sql
@@ -0,0 +1,10 @@
+﻿/*CREATE TABLE TIPO_JUEGO 
+(
+    tipo_juego_id	VARCHAR(512),
+    clave_tipo_juego	INT,
+    descripcion_tipo_juego	VARCHAR(512),
+    nombre_tipo_juego	VARCHAR(512)
+);*/
+
+INSERT INTO TIPO_JUEGO (tipo_juego_id, clave_tipo_juego, descripcion_tipo_juego, nombre_tipo_juego) VALUES ('1', '15126', 'Juegos con interaccion fisica', 'Juego de mesa');
+INSERT INTO TIPO_JUEGO (tipo_juego_id, clave_tipo_juego, descripcion_tipo_juego, nombre_tipo_juego) VALUES ('2', '66224', 'Juegos en ambientes virtuales', 'Juego digital');
