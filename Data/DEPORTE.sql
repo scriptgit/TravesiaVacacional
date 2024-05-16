6fe3ce5c75c77260da0374823005c00823369d19
Ulises <38197638+scriptgit@users.noreply.github.com>
2024-05-16 15:18:34 -0600

    dataset tabla deporte

---
+++ Data/DEPORTE.sql
@@ -0,0 +1,17 @@
+﻿/*CREATE TABLE DEPORTE 
+(
+    actividad_id	INT,
+    horas_entrenamiento	INT,
+    nombre_deporte	VARCHAR(512),
+    tipo_deporte_id	VARCHAR(512)
+);*/
+
+INSERT INTO DEPORTE (actividad_id, horas_entrenamiento, nombre_deporte, tipo_deporte_id) VALUES ('1', '240', 'Basketball', '2');
+INSERT INTO DEPORTE (actividad_id, horas_entrenamiento, nombre_deporte, tipo_deporte_id) VALUES ('2', '240', 'Beisbol', '2');
+INSERT INTO DEPORTE (actividad_id, horas_entrenamiento, nombre_deporte, tipo_deporte_id) VALUES ('9', '240', 'Boxeo', '1');
+INSERT INTO DEPORTE (actividad_id, horas_entrenamiento, nombre_deporte, tipo_deporte_id) VALUES ('10', '240', 'Danza', '3');
+INSERT INTO DEPORTE (actividad_id, horas_entrenamiento, nombre_deporte, tipo_deporte_id) VALUES ('11', '120', 'Esqui', '1');
+INSERT INTO DEPORTE (actividad_id, horas_entrenamiento, nombre_deporte, tipo_deporte_id) VALUES ('12', '240', 'Futbol', '2');
+INSERT INTO DEPORTE (actividad_id, horas_entrenamiento, nombre_deporte, tipo_deporte_id) VALUES ('16', '120', 'Natacion', '3');
+INSERT INTO DEPORTE (actividad_id, horas_entrenamiento, nombre_deporte, tipo_deporte_id) VALUES ('19', '240', 'Tenis', '3');
+INSERT INTO DEPORTE (actividad_id, horas_entrenamiento, nombre_deporte, tipo_deporte_id) VALUES ('20', '240', 'Tiro con arco', '1');
