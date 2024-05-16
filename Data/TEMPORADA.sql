cbf76c6f601177aa3c874b9cd09f8a3b507b318f
Ulises <38197638+scriptgit@users.noreply.github.com>
2024-05-16 15:12:27 -0600

    DATASET tabla temporada

---
+++ Data/TEMPORADA.sql
@@ -0,0 +1,10 @@
+﻿/*CREATE TABLE TEMPORADA 
+(
+    temporada_id	INT,
+    descripcion	VARCHAR(512)
+);*/
+
+INSERT INTO TEMPORADA (temporada_id, descripcion) VALUES ('1', 'primavera');
+INSERT INTO TEMPORADA (temporada_id, descripcion) VALUES ('2', 'verano');
+INSERT INTO TEMPORADA (temporada_id, descripcion) VALUES ('3', 'otoño');
+INSERT INTO TEMPORADA (temporada_id, descripcion) VALUES ('4', 'invierno');
