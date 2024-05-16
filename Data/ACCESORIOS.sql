53901642b381440150a2f98980b8f503ec02f378
Ulises <38197638+scriptgit@users.noreply.github.com>
2024-05-16 15:08:43 -0600

    dataset tabla accesorios

---
+++ Data/ACCESORIOS.sql
@@ -0,0 +1,17 @@
+﻿/*CREATE TABLE ACCESORIOS 
+(
+    accesorio_id	INT,
+    actividad_id	INT,
+    nombre_accesorio	VARCHAR(512)
+);*/
+
+INSERT INTO ACCESORIOS (accesorio_id, actividad_id, nombre_accesorio) VALUES ('1', '12', 'guantes de futbol');
+INSERT INTO ACCESORIOS (accesorio_id, actividad_id, nombre_accesorio) VALUES ('2', '16', 'lentes');
+INSERT INTO ACCESORIOS (accesorio_id, actividad_id, nombre_accesorio) VALUES ('3', '10', 'zapatos');
+INSERT INTO ACCESORIOS (accesorio_id, actividad_id, nombre_accesorio) VALUES ('4', '18', 'tienda de campania');
+INSERT INTO ACCESORIOS (accesorio_id, actividad_id, nombre_accesorio) VALUES ('5', '4', 'saco de dormir');
+INSERT INTO ACCESORIOS (accesorio_id, actividad_id, nombre_accesorio) VALUES ('6', '11', 'ropa para esquiar');
+INSERT INTO ACCESORIOS (accesorio_id, actividad_id, nombre_accesorio) VALUES ('7', '19', 'raqueta');
+INSERT INTO ACCESORIOS (accesorio_id, actividad_id, nombre_accesorio) VALUES ('8', '9', 'guantes de box');
+INSERT INTO ACCESORIOS (accesorio_id, actividad_id, nombre_accesorio) VALUES ('9', '2', 'guantes de beisbol');
+INSERT INTO ACCESORIOS (accesorio_id, actividad_id, nombre_accesorio) VALUES ('10', '20', 'arco');
