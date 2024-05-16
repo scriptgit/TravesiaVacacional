ccafbc499a08fcd7c0527ad27cab4e7d5fca5b1c
Ulises <38197638+scriptgit@users.noreply.github.com>
2024-05-16 12:56:25 -0600

    PDB1
    Tablas de la pdb1

---
+++ Data/VERSION_LIDER.sql
@@ -0,0 +1,58 @@
+﻿CREATE TABLE VERSION_LIDER 
+(
+    version_lider_id	INT,
+    version_id	INT,
+    empleado_id	INT,
+    fecha_obtencion	VARCHAR(512)
+);
+
+INSERT INTO VERSION_LIDER (version_lider_id, version_id, empleado_id, fecha_obtencion) VALUES ('1', '82', '828', '06/03/2017');
+INSERT INTO VERSION_LIDER (version_lider_id, version_id, empleado_id, fecha_obtencion) VALUES ('2', '26', '425', '29/08/2022');
+INSERT INTO VERSION_LIDER (version_lider_id, version_id, empleado_id, fecha_obtencion) VALUES ('3', '87', '356', '15/09/2011');
+INSERT INTO VERSION_LIDER (version_lider_id, version_id, empleado_id, fecha_obtencion) VALUES ('4', '81', '48', '30/07/2013');
+INSERT INTO VERSION_LIDER (version_lider_id, version_id, empleado_id, fecha_obtencion) VALUES ('5', '52', '421', '15/01/2024');
+INSERT INTO VERSION_LIDER (version_lider_id, version_id, empleado_id, fecha_obtencion) VALUES ('6', '39', '102', '04/12/2021');
+INSERT INTO VERSION_LIDER (version_lider_id, version_id, empleado_id, fecha_obtencion) VALUES ('7', '16', '639', '04/08/2015');
+INSERT INTO VERSION_LIDER (version_lider_id, version_id, empleado_id, fecha_obtencion) VALUES ('8', '58', '651', '24/04/2011');
+INSERT INTO VERSION_LIDER (version_lider_id, version_id, empleado_id, fecha_obtencion) VALUES ('9', '49', '804', '07/11/2011');
+INSERT INTO VERSION_LIDER (version_lider_id, version_id, empleado_id, fecha_obtencion) VALUES ('10', '84', '244', '03/01/2018');
+INSERT INTO VERSION_LIDER (version_lider_id, version_id, empleado_id, fecha_obtencion) VALUES ('11', '82', '964', '14/03/2013');
+INSERT INTO VERSION_LIDER (version_lider_id, version_id, empleado_id, fecha_obtencion) VALUES ('12', '15', '636', '26/07/2010');
+INSERT INTO VERSION_LIDER (version_lider_id, version_id, empleado_id, fecha_obtencion) VALUES ('13', '93', '377', '08/10/2017');
+INSERT INTO VERSION_LIDER (version_lider_id, version_id, empleado_id, fecha_obtencion) VALUES ('14', '77', '377', '18/06/2018');
+INSERT INTO VERSION_LIDER (version_lider_id, version_id, empleado_id, fecha_obtencion) VALUES ('15', '7', '780', '06/06/2016');
+INSERT INTO VERSION_LIDER (version_lider_id, version_id, empleado_id, fecha_obtencion) VALUES ('16', '86', '681', '03/07/2019');
+INSERT INTO VERSION_LIDER (version_lider_id, version_id, empleado_id, fecha_obtencion) VALUES ('17', '39', '787', '23/11/2013');
+INSERT INTO VERSION_LIDER (version_lider_id, version_id, empleado_id, fecha_obtencion) VALUES ('18', '77', '265', '11/01/2017');
+INSERT INTO VERSION_LIDER (version_lider_id, version_id, empleado_id, fecha_obtencion) VALUES ('19', '25', '37', '23/06/2015');
+INSERT INTO VERSION_LIDER (version_lider_id, version_id, empleado_id, fecha_obtencion) VALUES ('20', '17', '817', '06/08/2020');
+INSERT INTO VERSION_LIDER (version_lider_id, version_id, empleado_id, fecha_obtencion) VALUES ('21', '24', '643', '21/09/2012');
+INSERT INTO VERSION_LIDER (version_lider_id, version_id, empleado_id, fecha_obtencion) VALUES ('22', '24', '610', '19/12/2021');
+INSERT INTO VERSION_LIDER (version_lider_id, version_id, empleado_id, fecha_obtencion) VALUES ('23', '15', '578', '20/05/2020');
+INSERT INTO VERSION_LIDER (version_lider_id, version_id, empleado_id, fecha_obtencion) VALUES ('24', '5', '107', '15/12/2020');
+INSERT INTO VERSION_LIDER (version_lider_id, version_id, empleado_id, fecha_obtencion) VALUES ('25', '14', '422', '08/06/2014');
+INSERT INTO VERSION_LIDER (version_lider_id, version_id, empleado_id, fecha_obtencion) VALUES ('26', '37', '486', '16/04/2023');
+INSERT INTO VERSION_LIDER (version_lider_id, version_id, empleado_id, fecha_obtencion) VALUES ('27', '15', '472', '20/04/2015');
+INSERT INTO VERSION_LIDER (version_lider_id, version_id, empleado_id, fecha_obtencion) VALUES ('28', '58', '194', '14/05/2023');
+INSERT INTO VERSION_LIDER (version_lider_id, version_id, empleado_id, fecha_obtencion) VALUES ('29', '19', '619', '13/03/2014');
+INSERT INTO VERSION_LIDER (version_lider_id, version_id, empleado_id, fecha_obtencion) VALUES ('30', '72', '103', '29/04/2015');
+INSERT INTO VERSION_LIDER (version_lider_id, version_id, empleado_id, fecha_obtencion) VALUES ('31', '97', '416', '16/10/2014');
+INSERT INTO VERSION_LIDER (version_lider_id, version_id, empleado_id, fecha_obtencion) VALUES ('32', '54', '501', '23/11/2019');
+INSERT INTO VERSION_LIDER (version_lider_id, version_id, empleado_id, fecha_obtencion) VALUES ('33', '97', '255', '06/08/2019');
+INSERT INTO VERSION_LIDER (version_lider_id, version_id, empleado_id, fecha_obtencion) VALUES ('34', '65', '307', '22/10/2022');
+INSERT INTO VERSION_LIDER (version_lider_id, version_id, empleado_id, fecha_obtencion) VALUES ('35', '32', '501', '07/12/2015');
+INSERT INTO VERSION_LIDER (version_lider_id, version_id, empleado_id, fecha_obtencion) VALUES ('36', '84', '4', '15/04/2011');
+INSERT INTO VERSION_LIDER (version_lider_id, version_id, empleado_id, fecha_obtencion) VALUES ('37', '1', '986', '24/03/2014');
+INSERT INTO VERSION_LIDER (version_lider_id, version_id, empleado_id, fecha_obtencion) VALUES ('38', '6', '314', '23/03/2018');
+INSERT INTO VERSION_LIDER (version_lider_id, version_id, empleado_id, fecha_obtencion) VALUES ('39', '44', '589', '22/02/2016');
+INSERT INTO VERSION_LIDER (version_lider_id, version_id, empleado_id, fecha_obtencion) VALUES ('40', '21', '913', '02/05/2015');
+INSERT INTO VERSION_LIDER (version_lider_id, version_id, empleado_id, fecha_obtencion) VALUES ('41', '84', '970', '01/08/2016');
+INSERT INTO VERSION_LIDER (version_lider_id, version_id, empleado_id, fecha_obtencion) VALUES ('42', '16', '540', '16/07/2013');
+INSERT INTO VERSION_LIDER (version_lider_id, version_id, empleado_id, fecha_obtencion) VALUES ('43', '61', '700', '27/07/2023');
+INSERT INTO VERSION_LIDER (version_lider_id, version_id, empleado_id, fecha_obtencion) VALUES ('44', '34', '529', '10/01/2012');
+INSERT INTO VERSION_LIDER (version_lider_id, version_id, empleado_id, fecha_obtencion) VALUES ('45', '60', '6', '26/03/2024');
+INSERT INTO VERSION_LIDER (version_lider_id, version_id, empleado_id, fecha_obtencion) VALUES ('46', '15', '621', '27/09/2018');
+INSERT INTO VERSION_LIDER (version_lider_id, version_id, empleado_id, fecha_obtencion) VALUES ('47', '79', '704', '12/10/2010');
+INSERT INTO VERSION_LIDER (version_lider_id, version_id, empleado_id, fecha_obtencion) VALUES ('48', '82', '106', '18/07/2011');
+INSERT INTO VERSION_LIDER (version_lider_id, version_id, empleado_id, fecha_obtencion) VALUES ('49', '88', '235', '08/03/2018');
+INSERT INTO VERSION_LIDER (version_lider_id, version_id, empleado_id, fecha_obtencion) VALUES ('50', '12', '747', '14/10/2013');
