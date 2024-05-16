c8f93124694f129e432457c898aa7246de7844e8
Ulises <38197638+scriptgit@users.noreply.github.com>
2024-05-16 17:31:08 -0600

    ESTATUS_MEMBRESIA DATASET

---
+++ Data/ESTATUS_MEMBRESIA.sql
@@ -0,0 +1,10 @@
+﻿/*CREATE TABLE ESTATUS_MEMBRESIA 
+(
+    estatus_membresia_id	INT,
+    estatus_nombre	VARCHAR(512)
+);*/
+
+INSERT INTO ESTATUS_MEMBRESIA (estatus_membresia_id, estatus_nombre) VALUES ('1', 'VIGENTE');
+INSERT INTO ESTATUS_MEMBRESIA (estatus_membresia_id, estatus_nombre) VALUES ('2', 'CON ADEUDO');
+INSERT INTO ESTATUS_MEMBRESIA (estatus_membresia_id, estatus_nombre) VALUES ('3', 'SUSPENDIDA');
+INSERT INTO ESTATUS_MEMBRESIA (estatus_membresia_id, estatus_nombre) VALUES ('4', 'CANCELADA');
