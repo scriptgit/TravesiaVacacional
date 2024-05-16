da0b6f19df710daedd42d3001203ab3b2c84eb90
Ulises <38197638+scriptgit@users.noreply.github.com>
2024-05-16 12:57:09 -0600

    Add files via upload
    PDB1

---
+++ Data/LIDER.sql
@@ -0,0 +1,57 @@
+﻿CREATE TABLE LIDER 
+(
+    empleado_id	INT,
+    clave_lider	INT,
+    anios_experiencia	INT
+);
+
+INSERT INTO LIDER (empleado_id, clave_lider, anios_experiencia) VALUES ('828', '90402', '6');
+INSERT INTO LIDER (empleado_id, clave_lider, anios_experiencia) VALUES ('425', '78268', '5');
+INSERT INTO LIDER (empleado_id, clave_lider, anios_experiencia) VALUES ('356', '97980', '5');
+INSERT INTO LIDER (empleado_id, clave_lider, anios_experiencia) VALUES ('48', '18328', '6');
+INSERT INTO LIDER (empleado_id, clave_lider, anios_experiencia) VALUES ('421', '54008', '7');
+INSERT INTO LIDER (empleado_id, clave_lider, anios_experiencia) VALUES ('102', '69821', '6');
+INSERT INTO LIDER (empleado_id, clave_lider, anios_experiencia) VALUES ('639', '53391', '6');
+INSERT INTO LIDER (empleado_id, clave_lider, anios_experiencia) VALUES ('651', '37787', '6');
+INSERT INTO LIDER (empleado_id, clave_lider, anios_experiencia) VALUES ('804', '39736', '6');
+INSERT INTO LIDER (empleado_id, clave_lider, anios_experiencia) VALUES ('244', '83244', '5');
+INSERT INTO LIDER (empleado_id, clave_lider, anios_experiencia) VALUES ('964', '46544', '7');
+INSERT INTO LIDER (empleado_id, clave_lider, anios_experiencia) VALUES ('636', '69038', '6');
+INSERT INTO LIDER (empleado_id, clave_lider, anios_experiencia) VALUES ('377', '53516', '7');
+INSERT INTO LIDER (empleado_id, clave_lider, anios_experiencia) VALUES ('377', '72309', '6');
+INSERT INTO LIDER (empleado_id, clave_lider, anios_experiencia) VALUES ('780', '95642', '7');
+INSERT INTO LIDER (empleado_id, clave_lider, anios_experiencia) VALUES ('681', '23849', '7');
+INSERT INTO LIDER (empleado_id, clave_lider, anios_experiencia) VALUES ('787', '48307', '7');
+INSERT INTO LIDER (empleado_id, clave_lider, anios_experiencia) VALUES ('265', '45742', '7');
+INSERT INTO LIDER (empleado_id, clave_lider, anios_experiencia) VALUES ('37', '86063', '5');
+INSERT INTO LIDER (empleado_id, clave_lider, anios_experiencia) VALUES ('817', '48501', '7');
+INSERT INTO LIDER (empleado_id, clave_lider, anios_experiencia) VALUES ('643', '89152', '5');
+INSERT INTO LIDER (empleado_id, clave_lider, anios_experiencia) VALUES ('610', '56404', '6');
+INSERT INTO LIDER (empleado_id, clave_lider, anios_experiencia) VALUES ('578', '40735', '7');
+INSERT INTO LIDER (empleado_id, clave_lider, anios_experiencia) VALUES ('107', '76920', '5');
+INSERT INTO LIDER (empleado_id, clave_lider, anios_experiencia) VALUES ('422', '71820', '6');
+INSERT INTO LIDER (empleado_id, clave_lider, anios_experiencia) VALUES ('486', '49408', '6');
+INSERT INTO LIDER (empleado_id, clave_lider, anios_experiencia) VALUES ('472', '76222', '6');
+INSERT INTO LIDER (empleado_id, clave_lider, anios_experiencia) VALUES ('194', '88817', '6');
+INSERT INTO LIDER (empleado_id, clave_lider, anios_experiencia) VALUES ('619', '55174', '6');
+INSERT INTO LIDER (empleado_id, clave_lider, anios_experiencia) VALUES ('103', '75358', '7');
+INSERT INTO LIDER (empleado_id, clave_lider, anios_experiencia) VALUES ('416', '60923', '7');
+INSERT INTO LIDER (empleado_id, clave_lider, anios_experiencia) VALUES ('501', '84222', '7');
+INSERT INTO LIDER (empleado_id, clave_lider, anios_experiencia) VALUES ('255', '81251', '5');
+INSERT INTO LIDER (empleado_id, clave_lider, anios_experiencia) VALUES ('307', '49690', '7');
+INSERT INTO LIDER (empleado_id, clave_lider, anios_experiencia) VALUES ('501', '30744', '5');
+INSERT INTO LIDER (empleado_id, clave_lider, anios_experiencia) VALUES ('4', '31772', '5');
+INSERT INTO LIDER (empleado_id, clave_lider, anios_experiencia) VALUES ('986', '28858', '7');
+INSERT INTO LIDER (empleado_id, clave_lider, anios_experiencia) VALUES ('314', '50182', '6');
+INSERT INTO LIDER (empleado_id, clave_lider, anios_experiencia) VALUES ('589', '50539', '6');
+INSERT INTO LIDER (empleado_id, clave_lider, anios_experiencia) VALUES ('913', '62485', '5');
+INSERT INTO LIDER (empleado_id, clave_lider, anios_experiencia) VALUES ('970', '97723', '5');
+INSERT INTO LIDER (empleado_id, clave_lider, anios_experiencia) VALUES ('540', '78190', '7');
+INSERT INTO LIDER (empleado_id, clave_lider, anios_experiencia) VALUES ('700', '23245', '7');
+INSERT INTO LIDER (empleado_id, clave_lider, anios_experiencia) VALUES ('529', '27140', '6');
+INSERT INTO LIDER (empleado_id, clave_lider, anios_experiencia) VALUES ('6', '52815', '7');
+INSERT INTO LIDER (empleado_id, clave_lider, anios_experiencia) VALUES ('621', '42812', '6');
+INSERT INTO LIDER (empleado_id, clave_lider, anios_experiencia) VALUES ('704', '26850', '5');
+INSERT INTO LIDER (empleado_id, clave_lider, anios_experiencia) VALUES ('106', '97075', '7');
+INSERT INTO LIDER (empleado_id, clave_lider, anios_experiencia) VALUES ('235', '21409', '5');
+INSERT INTO LIDER (empleado_id, clave_lider, anios_experiencia) VALUES ('747', '50579', '5');
