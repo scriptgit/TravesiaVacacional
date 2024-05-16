35cefa101f02a331f77d650b5383285adaa4bde1
Ulises <38197638+scriptgit@users.noreply.github.com>
2024-05-16 15:21:15 -0600

    dataset tabla juego

---
+++ Data/JUEGO.sql
@@ -0,0 +1,13 @@
+﻿/*CREATE TABLE JUEGO 
+(
+    actividad_id	INT,
+    nombre_juego	VARCHAR(512),
+    num_participantes	VARCHAR(512),
+    descripcion_juego	VARCHAR(512),
+    tipo_juego_id	VARCHAR(512)
+);*/
+
+INSERT INTO JUEGO (actividad_id, nombre_juego, num_participantes, descripcion_juego, tipo_juego_id) VALUES ('3', 'Calabozos y mazmorras', '5', 'Partidas de rol de calabozos y mazmorras', '1');
+INSERT INTO JUEGO (actividad_id, nombre_juego, num_participantes, descripcion_juego, tipo_juego_id) VALUES ('14', 'Juegos de mesa', '3', 'Diversos juegos de mesa de la companias mas importantes', '1');
+INSERT INTO JUEGO (actividad_id, nombre_juego, num_participantes, descripcion_juego, tipo_juego_id) VALUES ('15', 'Juegos de rol', '5', 'Juegos de rol fuera de dungeons and dragons', '1');
+INSERT INTO JUEGO (actividad_id, nombre_juego, num_participantes, descripcion_juego, tipo_juego_id) VALUES ('16', 'Realidad virtual', '1', 'Experiencia interactiva en un ambiente virtual', '2');
