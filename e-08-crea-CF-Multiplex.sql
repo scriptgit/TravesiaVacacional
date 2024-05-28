a8edcccc99d3e39f12e56526924e33f977227f5c
Ulises <38197638+scriptgit@users.noreply.github.com>
2024-05-28 16:47:10 -0600

    Update e-08-crea-CF-Multiplex.sql
    Comentarios para el usuario Oracle

--- e-08-crea-CF-Multiplex.sql
+++ e-08-crea-CF-Multiplex.sql
@@ -34,12 +34,19 @@ SHUTDOWN IMMEDIATE
 
 -- con usuario oracle crear una copia del ya existente a las dos nuevas rutas control02 y control03
 
--- cp /travesia/disk-07/fast-recovery-area/TRAVDIP1/controlfile/o1_mf_m3xn99nd_.ctl /travesia/disk-02/app/oracle/oradata/TRAVDIP1/
--- mv /travesia/disk-02/app/oracle/oradata/TRAVDIP1/o1_mf_m3xn99nd_.ctl /travesia/disk-02/app/oracle/oradata/TRAVDIP1/control02.ctl
-
--- cp /travesia/disk-07/fast-recovery-area/TRAVDIP1/controlfile/o1_mf_m3xn99nd_.ctl /travesia/disk-03/app/oracle/oradata/TRAVDIP1/
+--Haciendo una copia del controlfile para el disk-02
+-- En un paso
+-- cp /travesia/disk-07/fast-recovery-area/TRAVDIP1/controlfile/nombre_del_controlfile.ctl /travesia/disk-02/app/oracle/oradata/TRAVDIP1/control02.ctl
+-- En dos pasos
+-- cp /travesia/disk-07/fast-recovery-area/TRAVDIP1/controlfile/nombre_del_controlfile.ctl /travesia/disk-02/app/oracle/oradata/TRAVDIP1/
+-- mv /travesia/disk-02/app/oracle/oradata/TRAVDIP1/nombre_del_controlfile.ctl /travesia/disk-02/app/oracle/oradata/TRAVDIP1/control02.ctl
+-- En caso de no tener la carpeta de controlfiles para el disk-03 crearla
 -- mkdir -p /travesia/disk-03/app/oracle/oradata/TRAVDIP1
--- mv /travesia/disk-03/app/oracle/oradata/TRAVDIP1/o1_mf_m3xn99nd_.ctl /travesia/disk-03/app/oracle/oradata/TRAVDIP1/control03.ctl
+-- Copia en un paso
+-- cp /travesia/disk-07/fast-recovery-area/TRAVDIP1/controlfile/nombre_del_controlfile.ctl /travesia/disk-03/app/oracle/oradata/TRAVDIP1/control03.ctl
+-- Copia en dos pasos
+-- cp /travesia/disk-07/fast-recovery-area/TRAVDIP1/controlfile/nombre_del_controlfile.ctl /travesia/disk-03/app/oracle/oradata/TRAVDIP1/
+-- mv /travesia/disk-03/app/oracle/oradata/TRAVDIP1/nombre_del_controlfile.ctl /travesia/disk-03/app/oracle/oradata/TRAVDIP1/control03.ctl
 Pause [Enter] para continuar, realizar los pasos solicitados como comentario con usuario oracle
 STARTUP
 
