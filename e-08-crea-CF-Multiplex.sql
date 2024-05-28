8bf4120a781ab0aad7c467d2460eee0e39908329
Ulises <38197638+scriptgit@users.noreply.github.com>
2024-05-28 16:34:30 -0600

    Update e-08-crea-CF-Multiplex.sql
    comentario

--- e-08-crea-CF-Multiplex.sql
+++ e-08-crea-CF-Multiplex.sql
@@ -40,7 +40,7 @@ SHUTDOWN IMMEDIATE
 -- cp /travesia/disk-07/fast-recovery-area/TRAVDIP1/controlfile/o1_mf_m3xn99nd_.ctl /travesia/disk-03/app/oracle/oradata/TRAVDIP1/
 -- mkdir -p /travesia/disk-03/app/oracle/oradata/TRAVDIP1
 -- mv /travesia/disk-03/app/oracle/oradata/TRAVDIP1/o1_mf_m3xn99nd_.ctl /travesia/disk-03/app/oracle/oradata/TRAVDIP1/control03.ctl
-Pause [Enter] para continuar
+Pause [Enter] para continuar, realizar los pasos solicitados como comentario con usuario oracle
 STARTUP
 
 PROMPT mostrando nombre y ubicación de los CONTROLFILE
