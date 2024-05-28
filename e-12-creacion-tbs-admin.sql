09d4a89a76e622aeb44efaddd99618720c6fb5d4
Ulises <38197638+scriptgit@users.noreply.github.com>
2024-05-28 17:41:33 -0600

    Update e-12-creacion-tbs-admin.sql
    Adaptando rutas al nombrado utilizado en scripts anteriores

--- e-12-creacion-tbs-admin.sql
+++ e-12-creacion-tbs-admin.sql
@@ -19,12 +19,29 @@ define pdb2_container='negocio_con'
 
 --TODO: añadir TBs de los indices
 
+Prompt inicio sesion en root
+alter session set container = cdb$root;
+
+Prompt Apertura del app container
+alter pluggable database &app_container open;
+
+Prompt Crear carpetas con usuario oracle en caso de no tenerlas:
+Prompt mkdir -p /travesia/disk-05/app/oracle/oradata/TRAVDIP1
+Prompt mkdir -p /travesia/disk-06/app/oracle/oradata/TRAVDIP1
+Prompt mkdir -p /travesia/disk-08/app/oracle/oradata/TRAVDIP1
+Pause [Enter] para continuar
+
+
 Prompt Cambiando sesión a &pdb1_container
 alter session set container = &pdb1_container;
 
+Prompt Apertura de las pdbs
+alter pluggable database &pdb1_container open;
+alter pluggable database &pdb2_container open;
+
 create tablespace ADMIN_TBS
     datafile 
-        '/travesia/disk_1/travesia/admin_tbs_01.dbf' size 1G
+        '/travesia/disk-01/app/oracle/oradata/TRAVDIP1/admin_tbs_01.dbf' size 1G
     autoextend on next 10M maxsize 10G
     autoallocate
     segment space management auto
@@ -32,9 +49,9 @@ create tablespace ADMIN_TBS
 
 create tablespace ADMIN_EMPLEADO_TBS
     datafile 
-        '/travesia/disk_1/travesia/admin_empleado_tbs_01.dbf' size 1G,
-        '/travesia/disk_2/travesia/admin_empleado_tbs_02.dbf' size 1G,
-        '/travesia/disk_3/travesia/admin_empleado_tbs_03.dbf' size 1G
+        '/travesia/disk-01/app/oracle/oradata/TRAVDIP1/admin_empleado_tbs_01.dbf' size 1G,
+        '/travesia/disk-02/app/oracle/oradata/TRAVDIP1/admin_empleado_tbs_02.dbf' size 1G,
+        '/travesia/disk-03/app/oracle/oradata/TRAVDIP1/admin_empleado_tbs_03.dbf' size 1G
     autoextend on next 30M maxsize 30G
     autoallocate
     segment space management auto
@@ -42,8 +59,8 @@ create tablespace ADMIN_EMPLEADO_TBS
 
 create tablespace ADMIN_INDICES_TBS
     datafile 
-        '/travesia/disk_2/travesia/admin_indices_tbs_01.dbf' size 1G,
-        '/travesia/disk_3/travesia/admin_indices_tbs_02.dbf' size 1G
+        '/travesia/disk-02/app/oracle/oradata/TRAVDIP1/admin_indices_tbs_01.dbf' size 1G,
+        '/travesia/disk-03/app/oracle/oradata/TRAVDIP1/admin_indices_tbs_02.dbf' size 1G
     autoextend on next 30M maxsize 15G
     autoallocate
     segment space management auto
@@ -56,9 +73,9 @@ alter session set container = &pdb2_container;
 
 create tablespace NEGOCIO_CLIENTE_TBS
     datafile 
-        '/travesia/disk_4/travesia/negocio_cliente_tbs_01.dbf' size 1G,
-        '/travesia/disk_5/travesia/negocio_cliente_tbs_02.dbf' size 1G,
-        '/travesia/disk_6/travesia/negocio_cliente_tbs_03.dbf' size 1G
+        '/travesia/disk-04/app/oracle/oradata/TRAVDIP1/negocio_cliente_tbs_01.dbf' size 1G,
+        '/travesia/disk-05/app/oracle/oradata/TRAVDIP1/negocio_cliente_tbs_02.dbf' size 1G,
+        '/travesia/disk-06/app/oracle/oradata/TRAVDIP1/negocio_cliente_tbs_03.dbf' size 1G
     autoextend on next 30M maxsize 30G
     autoallocate
     segment space management auto
@@ -66,8 +83,8 @@ create tablespace NEGOCIO_CLIENTE_TBS
 
 create tablespace NEGOCIO_MEMBRESIA_TBS
     datafile 
-        '/travesia/disk_4/travesia/negocio_membresia_tbs_01.dbf' size 1G,
-        '/travesia/disk_5/travesia/negocio_membresia_tbs_02.dbf' size 1G
+        '/travesia/disk-04/app/oracle/oradata/TRAVDIP1/negocio_membresia_tbs_01.dbf' size 1G,
+        '/travesia/disk-05/app/oracle/oradata/TRAVDIP1/negocio_membresia_tbs_02.dbf' size 1G
     autoextend on next 30M maxsize 30G
     autoallocate
     segment space management auto
@@ -75,7 +92,7 @@ create tablespace NEGOCIO_MEMBRESIA_TBS
 
 create tablespace NEGOCIO_ACTIVIDAD_TBS
     datafile 
-        '/travesia/disk_6/travesia/negocio_actividad_tbs_01.dbf' size 1G
+        '/travesia/disk-06/app/oracle/oradata/TRAVDIP1/negocio_actividad_tbs_01.dbf' size 1G
     autoextend on next 10M maxsize 15G
     autoallocate
     segment space management auto
@@ -83,7 +100,7 @@ create tablespace NEGOCIO_ACTIVIDAD_TBS
 
 create bigfile tablespace NEGOCIO_MEDIA_TBS
     datafile 
-        '/travesia/disk_8/travesia/negocio_media_tbs_01.dbf' size 10G
+        '/travesia/disk-08/app/oracle/oradata/TRAVDIP1/negocio_media_tbs_01.dbf' size 10G
     autoextend on next 1g maxsize 20G
     autoallocate
     segment space management auto
@@ -92,8 +109,8 @@ create bigfile tablespace NEGOCIO_MEDIA_TBS
 
 create tablespace NEGOCIO_INDICES_TBS
     datafile 
-        '/travesia/disk_4/travesia/negocio_indices_tbs_01.dbf' size 1G,
-        '/travesia/disk_5/travesia/negocio_indices_tbs_02.dbf' size 1G
+        '/travesia/disk-04/app/oracle/oradata/TRAVDIP1/negocio_indices_tbs_01.dbf' size 1G,
+        '/travesia/disk-05/app/oracle/oradata/TRAVDIP1/negocio_indices_tbs_02.dbf' size 1G
     autoextend on next 30M maxsize 15G
     autoallocate
     segment space management auto
