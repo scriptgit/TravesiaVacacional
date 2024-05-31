1a42031fcde43ee4b5ed3f733aaa943328239479
Ulises <38197638+scriptgit@users.noreply.github.com>
2024-05-31 13:05:54 -0600

    Update e-12-creacion-tbs-admin.sql
    Creación de tablespaces con el common user

--- e-12-creacion-tbs-admin.sql
+++ e-12-creacion-tbs-admin.sql
@@ -8,6 +8,8 @@
 define syslogon='sys/systemproy as sysdba'
 --define t_user='userproy'
 --define userlogon='&t_user/&t_user'
+define common_user='common_user'
+define common_user_logon='c##&common_user/&common_user'
 define app_admin='app_admin'
 define app_container='app_container'
 define pdb1_admin='admin_administracion_con'
@@ -20,7 +22,21 @@ define pdb2_container='negocio_con'
 --TODO: añadir TBs de los indices
 
 Prompt inicio sesion en root
+conn &syslogon
 alter session set container = cdb$root;
+
+
+
+Pause Creando common_user [Enter] para continuar
+CREATE USER c##common_user IDENTIFIED BY common_user CONTAINER=ALL;
+GRANT DBA TO c##common_user;
+GRANT SYSDBA TO c##common_user CONTAINER=ALL;
+GRANT CREATE SESSION TO c##common_user CONTAINER=ALL;
+GRANT CREATE ANY TABLE, ALTER ANY TABLE, DROP ANY TABLE TO c##common_user;
+GRANT CREATE ANY INDEX, ALTER ANY INDEX, DROP ANY INDEX TO c##common_user;
+GRANT CREATE ANY SEQUENCE, ALTER ANY SEQUENCE, DROP ANY SEQUENCE TO c##common_user;
+GRANT CREATE TABLESPACE, ALTER TABLESPACE, DROP TABLESPACE TO c##common_user;
+
 
 Prompt Apertura del app container
 alter pluggable database &app_container open;
@@ -37,6 +53,12 @@ alter pluggable database &pdb2_container open;
 
 Prompt Cambiando sesión a &pdb1_container
 alter session set container = &pdb1_container;
+Prompt conectando como common_user
+conn &common_user_logon
+
+/*DROP TABLESPACE ADMIN_TBS INCLUDING CONTENTS AND DATAFILES;
+DROP TABLESPACE ADMIN_EMPLEADO_TBS INCLUDING CONTENTS AND DATAFILES;
+DROP TABLESPACE ADMIN_INDICES_TBS INCLUDING CONTENTS AND DATAFILES;*/
 
 create tablespace ADMIN_TBS
     datafile 
@@ -66,9 +88,18 @@ create tablespace ADMIN_INDICES_TBS
 ;
 
 
-
+Prompt volviendose a conectar como sys
+conn &syslogon
 Prompt Cambiando sesión a &pdb2_container
 alter session set container = &pdb2_container;
+Prompt conectando como common_user
+conn &common_user_logon
+
+/*DROP TABLESPACE NEGOCIO_CLIENTE_TBS INCLUDING CONTENTS AND DATAFILES;
+DROP TABLESPACE NEGOCIO_MEMBRESIA_TBS INCLUDING CONTENTS AND DATAFILES;
+DROP TABLESPACE NEGOCIO_ACTIVIDAD_TBS INCLUDING CONTENTS AND DATAFILES;
+DROP TABLESPACE NEGOCIO_MEDIA_TBS INCLUDING CONTENTS AND DATAFILES;
+DROP TABLESPACE NEGOCIO_INDICES_TBS INCLUDING CONTENTS AND DATAFILES;*/
 
 create tablespace NEGOCIO_CLIENTE_TBS
     datafile 
